ca398850e8b67cfb183d2a6ac80e48d1c1be6eb2
Ben Emmett <benjohnemmett@gmail.com>
2023-02-24 15:08:13 -0600

    extracted matrix data from hub75 class & added bouncer fix from danielmader.

--- bouncer.py
+++ bouncer.py
@@ -1,7 +1,32 @@
-
 class Bouncer():
+    '''
+    Bouncing image object.
+    
+    Parameters
+    ----------
+    x : int
+        Initial x position (top row) of image.
+    y : int
+        Initial y position (left col) of image.
+    width : int
+        Image width.
+    height : int
+        Image height.
+    max_x : int
+        Column limit right.
+    max_y : int
+        Row limit bottom.
+    min_x : int
+        Column limit left.
+    min_y : int
+        Row limit top.
+    dx : int
+        Initial x direction (in cols/cycle).
+    dy : int
+        Initial y direction (in rows/cycle).
+    '''
 
-    def __init__(self, x, y, height, width, max_x, max_y, dx=1, dy=1):
+    def __init__(self, x, y, width, height, max_x, max_y, min_x=0, min_y=0, dx=1, dy=1):
         self.x = x
         self.y = y
         self.height = height
@@ -11,21 +36,26 @@ class Bouncer():
 
         self.max_x = max_x
         self.max_y = max_y
+        self.min_x = min_x
+        self.min_y = min_y
 
-    def Update(self):
+    def update(self):
+        '''
+        Update object positions (x,y) and directions (dx,dy).
+
+        Returns
+        -------
+        None.
+        '''
         self.x += self.dx
         self.y += self.dy
 
         if (self.y > (self.max_y - self.height)):
             self.dy *= -1
-            self.y = self.max_y
-        elif (self.y < 0):
+        elif (self.y <= self.min_y) and (self.dy < 0):
             self.dy *= -1
-            self.y = 0
 
         if (self.x > (self.max_x - self.width)):
             self.dx *= -1
-            self.x = self.max_x
-        elif (self.x < 0):
+        elif (self.x <= self.min_x) and (self.dx < 0):
             self.dx *= -1
-            self.x = 0
--- hub75.py
+++ hub75.py
@@ -1,15 +1,12 @@
 from machine import Pin, SoftSPI, freq
 from time import sleep_us
+from matrixdata import MatrixData
 
 freq(240000000)
 
 class Hub75SpiConfiguration:
 
     def __init__(self):
-        self.MatrixRows = 32
-        self.HalfMatrixRows = 16
-        self.MatrixColumns = 64
-
         self.SpiMisoPinNumber = 13
         self.SpiBaudRate = 2500000
         
@@ -37,8 +34,11 @@ class Hub75SpiConfiguration:
 
 class Hub75Spi:
 
-    def __init__(self, config):
+    def __init__(self, matrix_data, config):
         self.config = config
+
+        self.matrix_data = matrix_data
+        self.half_row_size = matrix_data.row_size //2
 
         self.LatchPin = Pin(config.LatchPinNumber, Pin.OUT)
         self.OutputEnablePin = Pin(config.OutputEnablePinNumber, Pin.OUT)
@@ -60,13 +60,6 @@ class Hub75Spi:
         self.Green2Spi = SoftSPI(baudrate=config.SpiBaudRate, polarity=1, phase=0, sck=Pin(config.ClockPinNumber), mosi=Pin(config.Green2PinNumber), miso=Pin(config.SpiMisoPinNumber))
         self.Blue1Spi = SoftSPI(baudrate=config.SpiBaudRate, polarity=1, phase=0, sck=Pin(config.ClockPinNumber), mosi=Pin(config.Blue1PinNumber), miso=Pin(config.SpiMisoPinNumber))
         self.Blue2Spi = SoftSPI(baudrate=config.SpiBaudRate, polarity=1, phase=0, sck=Pin(config.ClockPinNumber), mosi=Pin(config.Blue2PinNumber), miso=Pin(config.SpiMisoPinNumber))
-
-        self.RedMatrixData = [bytearray(8) for x in range(32)]
-        self.GreenMatrixData = [bytearray(8) for x in range(32)]
-        self.BlueMatrixData = [bytearray(8) for x in range(32)]
-        
-        self.DirtyBytesArray = []
-        
         
     def SetRowSelect(self, row):
         self.LineSelectAPin.value(row & 1)
@@ -75,11 +68,10 @@ class Hub75Spi:
         self.LineSelectDPin.value(row & 8)
         self.LineSelectEPin.value(row & 16)
 
-
     def DisplayTopHalf(self):
-        for row in range(self.config.HalfMatrixRows):
+        for row in range(self.half_row_size):
             # shift in data
-            RowData = self.RedMatrixData[row]
+            RowData = self.matrix_data.red_matrix_data[row]
             self.Red1Spi.write(RowData)
             self.OutputEnablePin.on() # disable
 
@@ -91,7 +83,7 @@ class Hub75Spi:
             sleep_us(self.config.IlluminationTimeMicroseconds)
             
             # shift in data
-            RowData = self.GreenMatrixData[row]
+            RowData = self.matrix_data.green_matrix_data[row]
             self.Green1Spi.write(RowData)
             self.OutputEnablePin.on() # disable
             self.LatchPin.on()
@@ -100,7 +92,7 @@ class Hub75Spi:
             sleep_us(self.config.IlluminationTimeMicroseconds)
             
             # shift in data
-            RowData = self.BlueMatrixData[row]
+            RowData = self.matrix_data.blue_matrix_data[row]
             self.Blue1Spi.write(RowData)
             self.OutputEnablePin.on() # disable
             self.LatchPin.on()
@@ -110,22 +102,21 @@ class Hub75Spi:
             
         self.Blue1Spi.write(bytearray(8))
 
-
     def DisplayBottomHalf(self):
-        for row in range(self.config.HalfMatrixRows, self.config.MatrixRows):
+        for row in range(self.half_row_size, self.matrix_data.row_size):
             # shift in data
-            RowData = self.RedMatrixData[row]
+            RowData = self.matrix_data.red_matrix_data[row]
             self.Red2Spi.write(RowData)
             self.OutputEnablePin.on() # disable
 
-            self.SetRowSelect(row % self.config.HalfMatrixRows)
+            self.SetRowSelect(row % self.half_row_size)
 
             self.LatchPin.on()
             self.LatchPin.off()
             self.OutputEnablePin.off() # enable
             sleep_us(self.config.IlluminationTimeMicroseconds)
             
-            RowData = self.GreenMatrixData[row]
+            RowData = self.matrix_data.green_matrix_data[row]
             self.Green2Spi.write(RowData)
             self.OutputEnablePin.on() # disable
             self.LatchPin.on()
@@ -133,7 +124,7 @@ class Hub75Spi:
             self.OutputEnablePin.off() # enable
             sleep_us(self.config.IlluminationTimeMicroseconds)
                         
-            RowData = self.BlueMatrixData[row]
+            RowData = self.matrix_data.blue_matrix_data[row]
             self.Blue2Spi.write(RowData)
             self.OutputEnablePin.on() # disable
             self.LatchPin.on()
@@ -143,45 +134,7 @@ class Hub75Spi:
 
         self.Blue2Spi.write(bytearray(8))
 
-
     def DisplayData(self):
         self.DisplayTopHalf()
         self.DisplayBottomHalf()
         
-
-    def SetPixelValue(self, row, col, val):
-        if (self.IsOutOfBounds(row, col)):
-            return
-        
-        cIndex = col // 8
-        byteIndex = 7 - (col % 8)
-
-        if val & 4:
-            self.RedMatrixData[row][cIndex] |= (1 << byteIndex)
-        if val & 2:
-            self.GreenMatrixData[row][cIndex] |= (1 << byteIndex)
-        if val & 1:
-            self.BlueMatrixData[row][cIndex] |= (1 << byteIndex)
-        
-        self.DirtyBytesArray.append((row,col//8))
-    
-
-    def ClearDirtyBytes(self):
-        for index in self.DirtyBytesArray:
-            self.RedMatrixData[index[0]][index[1]] = 0
-            self.GreenMatrixData[index[0]][index[1]] = 0
-            self.BlueMatrixData[index[0]][index[1]] = 0
-            
-        self.DirtyBytesArray = []
-    
-
-    def SetPixels(self, row, col, array):
-        for r in range(len(array)):
-            for c in range(len(array[0])):
-                if array[r][c]:
-                    self.SetPixelValue(row + r, col + c, array[r][c])
-    
-
-    def IsOutOfBounds(self, row, col):
-        return (row < 0 or row >= self.config.MatrixRows or col < 0 or col >= self.config.MatrixColumns)
-
