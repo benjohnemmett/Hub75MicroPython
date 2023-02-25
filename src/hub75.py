07f31437bb2c045f199e5d8193228f4abe4ddbf2
Ben Emmett <benjohnemmett@gmail.com>
2023-02-25 12:51:57 -0600

    Fixed blue afterglow on bottom line. Also incorporated formatting cleanup & comments from danielmader.

--- src/hub75.py
+++ src/hub75.py
@@ -1,14 +1,18 @@
 from machine import Pin, SoftSPI, freq
 from time import sleep_us
-from matrixdata import MatrixData
 
+#freq(160000000)  # default NodeMCU ESP-32S v1.1
 freq(240000000)
 
 class Hub75SpiConfiguration:
+    '''
+    SoftSPI pin configuration for HUB75 RGB LED matrix.
+    Defaults are for Hub75 Blaster PCB from Hackerbox 0065
+    (https://hackerboxes.com/collections/past-hackerboxes/products/hackerbox-0065-realtime).
+    '''
     def __init__(self):
-        self.spi_miso_pin_number = 13
         self.spi_baud_rate = 2500000
-        
+
         self.illumination_time_microseconds = 10
 
         # Row select pins
@@ -28,15 +32,26 @@ class Hub75SpiConfiguration:
 
         self.clock_pin_number = 22
         self.latch_pin_number = 26
-        self.output_enable_pin_number = 25 # active low
+        self.output_enable_pin_number = 25  # active low
+
+        self.spi_miso_pin_number = 13  # not connected
 
 
 class Hub75Spi:
+    '''
+    HUB75 RGB LED matrix communication.
+    '''
     def __init__(self, matrix_data, config):
+        '''
+        Parameters
+        ----------
+        matrix_data : MatrixData object
+        config : Hub75SpiConfiguration
+            Pin configuration.
+        '''
         self.config = config
-
         self.matrix_data = matrix_data
-        self.half_row_size = matrix_data.row_size //2
+        self.half_row_size = matrix_data.row_size // 2
 
         self.latch_pin = Pin(config.latch_pin_number, Pin.OUT)
         self.output_enable_pin = Pin(config.output_enable_pin_number, Pin.OUT)
@@ -58,15 +73,27 @@ class Hub75Spi:
         self.green2_mosi_pin = Pin(config.green2_pin_number)
         self.blue1_mosi_pin = Pin(config.blue1_pin_number)
         self.blue2_mosi_pin = Pin(config.blue2_pin_number)
-        
+
         self.red1_spi = SoftSPI(baudrate=config.spi_baud_rate, polarity=1, phase=0, sck=Pin(config.clock_pin_number), mosi=self.red1_mosi_pin, miso=Pin(config.spi_miso_pin_number))
         self.red2_spi = SoftSPI(baudrate=config.spi_baud_rate, polarity=1, phase=0, sck=Pin(config.clock_pin_number), mosi=self.red2_mosi_pin, miso=Pin(config.spi_miso_pin_number))
         self.green1_spi = SoftSPI(baudrate=config.spi_baud_rate, polarity=1, phase=0, sck=Pin(config.clock_pin_number), mosi=self.green1_mosi_pin, miso=Pin(config.spi_miso_pin_number))
         self.green2_spi = SoftSPI(baudrate=config.spi_baud_rate, polarity=1, phase=0, sck=Pin(config.clock_pin_number), mosi=self.green2_mosi_pin, miso=Pin(config.spi_miso_pin_number))
         self.blue1_spi = SoftSPI(baudrate=config.spi_baud_rate, polarity=1, phase=0, sck=Pin(config.clock_pin_number), mosi=self.blue1_mosi_pin, miso=Pin(config.spi_miso_pin_number))
         self.blue2_spi = SoftSPI(baudrate=config.spi_baud_rate, polarity=1, phase=0, sck=Pin(config.clock_pin_number), mosi=self.blue2_mosi_pin, miso=Pin(config.spi_miso_pin_number))
-        
+
     def set_row_select(self, row):
+        '''
+        Set data for row select pins.
+
+        Parameters
+        ----------
+        row : int
+            current row for serial color data.
+
+        Returns
+        -------
+        None.
+        '''
         self.line_select_a_pin.value(row & 1)
         self.line_select_b_pin.value(row & 2)
         self.line_select_c_pin.value(row & 4)
@@ -74,6 +101,13 @@ class Hub75Spi:
         self.line_select_e_pin.value(row & 16)
 
     def display_top_half(self):
+        '''
+        Write top half of display, see display_data().
+
+        Returns
+        -------
+        None.
+        '''
         for row in range(self.half_row_size):
             # shift in data
             row_data = self.matrix_data.red_matrix_data[row]
@@ -87,7 +121,7 @@ class Hub75Spi:
             self.latch_pin.off()
             self.output_enable_pin.off() # enable
             sleep_us(self.config.illumination_time_microseconds)
-            
+
             # shift in data
             row_data = self.matrix_data.green_matrix_data[row]
             self.green1_spi.write(row_data)
@@ -97,7 +131,7 @@ class Hub75Spi:
             self.latch_pin.off()
             self.output_enable_pin.off() # enable
             sleep_us(self.config.illumination_time_microseconds)
-            
+
             # shift in data
             row_data = self.matrix_data.blue_matrix_data[row]
             self.blue1_spi.write(row_data)
@@ -109,6 +143,13 @@ class Hub75Spi:
             sleep_us(self.config.illumination_time_microseconds)
 
     def display_bottom_half(self):
+        '''
+        Write bottom half of display, see display_data().
+
+        Returns
+        -------
+        None.
+        '''
         for row in range(self.half_row_size, self.matrix_data.row_size):
             # shift in data
             row_data = self.matrix_data.red_matrix_data[row]
@@ -122,7 +163,7 @@ class Hub75Spi:
             self.latch_pin.off()
             self.output_enable_pin.off() # enable
             sleep_us(self.config.illumination_time_microseconds)
-            
+
             row_data = self.matrix_data.green_matrix_data[row]
             self.green2_spi.write(row_data)
             self.green2_mosi_pin.off()
@@ -131,7 +172,7 @@ class Hub75Spi:
             self.latch_pin.off()
             self.output_enable_pin.off() # enable
             sleep_us(self.config.illumination_time_microseconds)
-                        
+
             row_data = self.matrix_data.blue_matrix_data[row]
             self.blue2_spi.write(row_data)
             self.blue2_mosi_pin.off()
@@ -140,8 +181,20 @@ class Hub75Spi:
             self.latch_pin.off()
             self.output_enable_pin.off() # enable
             sleep_us(self.config.illumination_time_microseconds)
+        
+        #Flush out last blue line
+        self.blue2_spi.write(bytearray(8))
+        self.output_enable_pin.on()
+        self.latch_pin.on()
+        self.latch_pin.off()
 
     def display_data(self):
+        '''
+        Write pixel data to LED matrix.
+
+        Returns
+        -------
+        None.
+        '''
         self.display_top_half()
         self.display_bottom_half()
-        
