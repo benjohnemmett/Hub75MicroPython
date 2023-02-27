e2c4f8eeef1cc54b05992221a178999a5ac12fc8
MaDa <danielmader@users.noreply.github.com>
2023-02-27 12:28:19 +0100

    Docstrings added, indentation fixed.
    matrixdata.MatrixData.clear_all_bytes() added.
    hub75.Hub75Spi.display_bottom_half() fixed (automatic panel size).

--- src/main.py
+++ src/main.py
@@ -8,24 +8,63 @@ ROW_SIZE = 32
 COL_SIZE = 64
 
 config = hub75.Hub75SpiConfiguration()
+##-----------------------------------------------------------------------------
+## row select pins
+config.line_select_a_pin_number = 15
+config.line_select_b_pin_number = 2
+config.line_select_c_pin_number = 4
+config.line_select_d_pin_number = 16
+config.line_select_e_pin_number = 12
+## color data pins
+config.red1_pin_number = 32
+config.green1_pin_number = 33
+config.blue1_pin_number = 25
+config.red2_pin_number = 26
+config.green2_pin_number = 27
+config.blue2_pin_number = 14
+## logic pins
+config.clock_pin_number = 18
+config.latch_pin_number = 5
+config.output_enable_pin_number = 17  # active low
+config.spi_miso_pin_number = 13  # not connected
+## misc
+#config.illumination_time_microseconds = 10
+##-----------------------------------------------------------------------------
 matrix = matrixdata.MatrixData(ROW_SIZE, COL_SIZE)
 hub75spi = hub75.Hub75Spi(matrix, config)
 
 # Show Python Logo
 matrix.set_pixels(0, 16, logo)
-    
 for i in range(100):
     hub75spi.display_data()
 
 # Show bouncing objects
-earth_bounce = bouncer.Bouncer(0, 0, len(earth[0]), len(earth), COL_SIZE-1, ROW_SIZE-1, dx=2)
-saturn_bounce = bouncer.Bouncer(20, 10, len(saturn), len(saturn[0]), COL_SIZE-1, ROW_SIZE-1, dx=-2)
+earth_bounce = bouncer.Bouncer(0, 0, len(earth), len(earth[0]), 63, 31, dx=2)
+saturn_bounce = bouncer.Bouncer(20, 10, len(saturn), len(saturn[0]), 63, 31, dx=-2)
+square = [
+    [1,1,2,2,3,3,4,4,5,5,6,6,7,7],
+    [1,1,2,2,3,3,4,4,5,5,6,6,7,7],
+    [1,1,2,2,3,3,4,4,5,5,6,6,7,7],
+    [1,1,2,2,3,3,4,4,5,5,6,6,7,7],
+    [1,1,2,2,3,3,4,4,5,5,6,6,7,7],
+    [1,1,2,2,3,3,4,4,5,5,6,6,7,7],
+    [1,1,2,2,3,3,4,4,5,5,6,6,7,7],
+    [1,1,2,2,3,3,4,4,5,5,6,6,7,7],
+    [1,1,2,2,3,3,4,4,5,5,6,6,7,7],
+    [1,1,2,2,3,3,4,4,5,5,6,6,7,7],
+    [1,1,2,2,3,3,4,4,5,5,6,6,7,7],
+    [1,1,2,2,3,3,4,4,5,5,6,6,7,7],
+    [1,1,2,2,3,3,4,4,5,5,6,6,7,7],
+    [1,1,2,2,3,3,4,4,5,5,6,6,7,7]]
+square_bounce = bouncer.Bouncer(0,0, width=len(square[0]), height=len(square), max_x=63, max_y=31, min_x=0, min_y=0, dx=1, dy=1)
 
 while True:
     earth_bounce.update()
     saturn_bounce.update()
-    
+    square_bounce.update()
+
     matrix.clear_dirty_bytes()
     matrix.set_pixels(earth_bounce.y, earth_bounce.x, earth)
     matrix.set_pixels(saturn_bounce.y, saturn_bounce.x, saturn)
+    matrix.set_pixels(square_bounce.y, square_bounce.x, square)
     hub75spi.display_data()
--- src/matrixdata.py
+++ src/matrixdata.py
@@ -3,29 +3,79 @@ GREEN_VALUE = 2
 RED_VALUE = 4
 
 class MatrixData:
+    '''
+    RGB LED matrix data object for HUB75 LED displays.
+    3-Bit Color Mapping:
+        Color	Binary Value 	Decimal Value
+        Black	0b000			0
+        Blue	0b001			1
+        Green	0b010			2
+        Cyan	0b011			3
+        Red		0b100			4
+        Magenta	0b101			5
+        Yellow	0b110			6
+        White	0b111			7
+    '''
     def __init__(self, row_size, col_size):
         self.row_size = row_size
         self.col_size = col_size
-
         self.col_bytes = col_size // 8
 
-        self.red_matrix_data = [bytearray(self.col_bytes) for x in range(row_size)]
-        self.green_matrix_data = [bytearray(self.col_bytes) for x in range(row_size)]
-        self.blue_matrix_data = [bytearray(self.col_bytes) for x in range(row_size)]
-        
+        self.red_matrix_data = [bytearray(self.col_bytes) for x in range(self.row_size)]
+        self.green_matrix_data = [bytearray(self.col_bytes) for x in range(self.row_size)]
+        self.blue_matrix_data = [bytearray(self.col_bytes) for x in range(self.row_size)]
+        # self.red_matrix_data = [bytearray(self.col_bytes)] * self.row_size
+        # self.green_matrix_data = [bytearray(self.col_bytes)] * self.row_size
+        # self.blue_matrix_data = [bytearray(self.col_bytes)] * self.row_size
+
         self.dirty_bytes_array = []
 
     def set_pixels(self, row, col, array):
+        '''
+        Set an array of pixels (i.e. an image).
+
+        Parameters
+        ----------
+        row : int
+            Top row (y position) of image.
+        col : int
+            Left column (x position) of image.
+        array : 2D list of bitmapped values [[int,...],[...],...]
+            Decimal color representations of the image.
+
+        Returns
+        -------
+        None.
+        '''
         for r in range(len(array)):
             for c in range(len(array[0])):
                 if array[r][c]:
                     self.set_pixel_value(row + r, col + c, array[r][c])
 
     def set_pixel_value(self, row, col, val):
+        '''
+        Set an individual pixel.
+
+        Parameters
+        ----------
+        row : int
+            Row value of pixel (y value).
+        col : int
+            Column value of pixel (x value).
+        val : int
+            Decimal color representation.
+
+        Returns
+        -------
+        None.
+        '''
         if (self.is_out_of_bounds(row, col)):
             return
-        
+
+        # One row is divided into col_size // 8 = 8 bytes.
+        # => Compute the column index (i.e. byte) of the pixel.
         cIndex = col // 8
+        # => Compute the byte index of the pixel within the byte.
         byteIndex = 7 - (col % 8)
 
         if val & RED_VALUE:
@@ -34,16 +84,55 @@ class MatrixData:
             self.green_matrix_data[row][cIndex] |= (1 << byteIndex)
         if val & BLUE_VALUE:
             self.blue_matrix_data[row][cIndex] |= (1 << byteIndex)
-        
-        self.dirty_bytes_array.append((row,col//8))
-    
+
+        self.dirty_bytes_array.append((row, col//8))
+
     def is_out_of_bounds(self, row, col):
+        '''
+        Check if a pixel is out of LED matrix bounds.
+
+        Parameters
+        ----------
+        row : int
+            Row value of pixel (y value).
+        col : int
+            Column value of pixel (x value).
+
+        Returns
+        -------
+        bool
+            Pixel is out of bounds?
+        '''
         return (row < 0 or row >= self.row_size or col < 0 or col >= self.col_size)
 
     def clear_dirty_bytes(self):
+        '''
+        Reset dirty pixels.
+
+        Returns
+        -------
+        None.
+        '''
         for index in self.dirty_bytes_array:
             self.red_matrix_data[index[0]][index[1]] = 0
             self.green_matrix_data[index[0]][index[1]] = 0
             self.blue_matrix_data[index[0]][index[1]] = 0
-            
+
+        self.dirty_bytes_array = []
+
+    def clear_all_bytes(self):
+        '''
+        Reset all pixels.
+
+        Returns
+        -------
+        None.
+        '''
+        self.red_matrix_data = [bytearray(self.col_bytes) for x in range(self.row_size)]
+        self.green_matrix_data = [bytearray(self.col_bytes) for x in range(self.row_size)]
+        self.blue_matrix_data = [bytearray(self.col_bytes) for x in range(self.row_size)]
+        # self.red_matrix_data = [bytearray(self.col_bytes)] * self.row_size
+        # self.green_matrix_data = [bytearray(self.col_bytes)] * self.row_size
+        # self.blue_matrix_data = [bytearray(self.col_bytes)] * self.row_size
+
         self.dirty_bytes_array = []
